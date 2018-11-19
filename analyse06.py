9265cee812c4613c6de7e70960d8c032d8d30559
Vivien Geoffroy <mon email>
2018-11-19 19:04:27 +0100

    improvement of bus classificator

--- analyse06.py
+++ analyse06.py
@@ -8,61 +8,69 @@ Created on Sun Nov 18 18:44:30 2018
 
 import pandas
 from matplotlib import pyplot as plt
+import numpy as np
 
 columns = ['idscrap','time_saved','minutes','theorique']
 
-#data = pandas.read_csv('data200.csv',usecols = columns,parse_dates=['time_saved'])
-data['bus_id'] = pandas.Series(0,index = data.index)
-#plt.plot_date(data['time_saved'],data['minutes'],'+')
+data = pandas.read_csv('data200.csv',usecols = columns,parse_dates=['time_saved'])
 
-buses = []
-#for i in range(1,data['idscrap'].max()-1):
-#
-#    current = data.loc[data['idscrap']==i]
-#    previous = data.loc[data['idscrap']==i-1]
-#    suivant = data.loc[data['idscrap']==i+1]
-#    for classement in range(len(current)):
-        
-#
-#current = data.loc[data['idscrap']==1]
-#    for classement in range(len(current)):
-#        complete = False
-#        while not complete:
-#            
-            
-
-data2 = data.copy()
-
-while len(data2)!=0:
-    data2['rank'] = data2.groupby('idscrap')['minutes'].rank(ascending=True)
-    cur = data2.loc[data2['rank']==1]
-    i = 0
-    current_bus = []
-    bus_id = data['bus_id'].max() + 1
-    if len(cur>2):
-        while cur.iloc[0]['minutes']>2 or cur.iloc[1]['minutes']-cur.iloc[0]['minutes']<2:
-            data.loc[data['time_saved']==cur.iloc[0]['time_saved'],'bus_id'] = bus_id
-            data2 = data2.drop(cur.index[0])
-            cur = cur.drop(cur.index[0])
-            print(cur)
-            if len(cur) == 1 or cur.iloc[1]['minutes']-cur.iloc[0]['minutes']>2: 
-                  data.loc[data['time_saved']==cur.iloc[0]['time_saved'],'bus_id'] = bus_id
-
-                  data2 = data2.drop(cur.index[0])
-                  cur = cur.drop(cur.index[0])
-
-                  break
-
+def identifie_bus(data):
+    data['bus_id'] = pandas.Series(0,index = data.index)
+    data2 = data.copy()
+    
+    while len(data2)!=0:
+        data2['rank'] = data2.groupby('idscrap')['minutes'].rank(ascending=True)
+        cur = data2.loc[data2['rank']==1]
+        bus_id = data['bus_id'].max() + 1
+        if len(cur>2):
+            while cur.iloc[0]['minutes']>2 or cur.iloc[1]['minutes']-cur.iloc[0]['minutes']<2\
+            or  (cur.iloc[1]['time_saved']-cur.iloc[0]['time_saved']).seconds<60*cur.iloc[0]['minutes']:
+                data.loc[data['time_saved']==cur.iloc[0]['time_saved'],'bus_id'] = bus_id
+                data2 = data2.drop(cur.index[0])
+                cur = cur.drop(cur.index[0])
+                if len(cur) == 1 or cur.iloc[1]['minutes']-cur.iloc[0]['minutes']>2\
+                or  (cur.iloc[1]['time_saved']-cur.iloc[0]['time_saved']).seconds>60*cur.iloc[0]['minutes']: 
+                      data.loc[data['time_saved']==cur.iloc[0]['time_saved'],'bus_id'] = bus_id
+    
+                      data2 = data2.drop(cur.index[0])
+                      cur = cur.drop(cur.index[0])
+    
+                      break
+            print('bus added ' )
 
     
-#        data.loc[data['time_saved']==cur.iloc[0]['time_saved'],'bus_id'] = bus_id
-#        data2 = data2.drop(cur.index[0])
-    print('bus added ' )
+    return data
 
-for i in list(set(data['bus_id'])):
-    slic = data.loc[data['bus_id']==i]
-    plt.plot_date(slic['time_saved'],slic['minutes'],'+')
+def plot():
+    for i in list(set(data['bus_id'])):
+        slic = data.loc[data['bus_id']==i]
+        plt.plot_date(slic['time_saved'],slic['minutes'],'+',label=str(i))
+
+
+data = identifie_bus(data)
+
+
+def f(x):
+    return x['time_saved'].max()
+arrives = data.groupby('bus_id')['minutes'].min()<2
+heures = data.groupby('bus_id')['time_saved'].max()
+def summarize(index):
+    if arrives.loc[index]: return (index,heures[index])
+    else : return (index,None)
+a = data.groupby('bus_id').agg(f)
+arrivees = pandas.DataFrame(list(arrives.index.map(summarize)),columns = ['bus_id','arrivees'])
+data = data.merge(arrivees,on = 'bus_id')
+
+
+data = data.loc[-data['arrivees'].isnull()]
+data['real_minutes'] = data['arrivees'] - data['time_saved']
+data['real_minutes'] = data['real_minutes'].map(lambda x:x.seconds/60)
+
+for name,bus in data.groupby('bus_id'):
+    print(np.polyfit(bus['real_minutes'],bus['minutes'],deg = 1))
+
     
+
     
 
 
