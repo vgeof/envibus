906a14fb4d7a26c06d8b8cc63644513979530ce4
U-AMAIISDOM\vgeoffroy <vgeoffroy@NCEL71421.iis.amadeus.net>
2018-11-28 17:42:21 +0100

    algo corrected

--- analyse06.py
+++ analyse06.py
@@ -12,32 +12,40 @@ import numpy as np
 
 columns = ['idscrap','time_saved','minutes','theorique']
 
-data = pandas.read_csv('data200.csv',usecols = columns,parse_dates=['time_saved'])
+data = pandas.read_csv('data200current.csv',usecols = columns,parse_dates=['time_saved'])
 data = data.loc[data['theorique']==False]
-plt.plot_date('time_saved','minutes','+',data = data)
+data = data.loc[data['time_saved']>="2018-11-25 18:51:03.401308"]
+data = data.loc[data['time_saved']<="2018-11-27 18:51:03.401308"]
 
+plt.plot_date('time_saved','minutes','+',data = data)
+data['rank'] = data.groupby('idscrap')['minutes'].rank(ascending=True,method = 'first')
 def identifie_bus(data):
     data['bus_id'] = pandas.Series(0,index = data.index)
     data2 = data.copy()
     
     while len(data2)!=0:
-        data2['rank'] = data2.groupby('idscrap')['minutes'].rank(ascending=True)
+        data2['rank'] = data2.groupby('idscrap')['minutes'].rank(ascending=True,method = 'first')
         cur = data2.loc[data2['rank']==1]
         bus_id = data['bus_id'].max() + 1
         if len(cur>2):
-            while cur.iloc[0]['minutes']>2 or cur.iloc[1]['minutes']-cur.iloc[0]['minutes']<2\
+            while cur.iloc[0]['minutes']>0 or cur.iloc[1]['minutes']-cur.iloc[0]['minutes']<1\
             or  (cur.iloc[1]['time_saved']-cur.iloc[0]['time_saved']).seconds<60*cur.iloc[0]['minutes']:
-                data.loc[data['time_saved']==cur.iloc[0]['time_saved'],'bus_id'] = bus_id
+                plt.clf()
+                plot()
+                plt.pause(0.05)
+                data.loc[cur.iloc[0].name,'bus_id'] = bus_id
                 data2 = data2.drop(cur.index[0])
+
+                #input("Press")
                 cur = cur.drop(cur.index[0])
-                if len(cur) == 1 or cur.iloc[1]['minutes']-cur.iloc[0]['minutes']>2\
-                or  (cur.iloc[1]['time_saved']-cur.iloc[0]['time_saved']).seconds>60*cur.iloc[0]['minutes']: 
-                      data.loc[data['time_saved']==cur.iloc[0]['time_saved'],'bus_id'] = bus_id
-    
+                if len(cur) == 1 or (cur.iloc[0]['minutes'] == 0 and cur.iloc[1]['minutes']-cur.iloc[0]['minutes']>1) \
+                or  ((cur.iloc[1]['time_saved']-cur.iloc[0]['time_saved']).seconds>60*cur.iloc[0]['minutes'] and cur.iloc[0]['minutes']>0): 
+                      data.loc[cur.iloc[0].name,'bus_id'] = bus_id    
                       data2 = data2.drop(cur.index[0])
                       cur = cur.drop(cur.index[0])
     
                       break
+
             print('bus added ' )
 
     
