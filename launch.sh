edd86fa8d11e37fc581b165c45eb03026e8d7ca5
Vivien GEOFFROY <vgeoffroy@ncelrnd2442.nce.amadeus.net>
2019-02-01 20:35:13 +0100

    linux launch

---
+++ launch.sh
@@ -0,0 +1,27 @@
+cd /home/vgeoffroy/Code/envibus
+trap exit 1 2 3 6
+python lignes06.py 490&
+sleep 2
+python lignes06.py 224&
+sleep 2
+python lignes06.py 238&
+sleep 2
+python lignes06.py 208&
+sleep 2
+python lignes06.py 207&
+sleep 2
+python lignes06.py 236&
+sleep 2
+python lignes06.py 235&
+sleep 2
+python lignes06.py 218&
+sleep 2
+python lignes06.py 233&
+sleep 2
+python lignes06.py 227&
+sleep 2
+python lignes06.py 229&
+sleep 2
+python lignes06.py 241&
+sleep 2 
+python lignes06.py 230&
